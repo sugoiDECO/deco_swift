b31402e9149ed7ca8515f33020b758f494eb3224
Shinnosuke Komiya <kurohune538@gmail.com>
2015-12-14 12:15:15 +0900

    add Libraries

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'deco' do
+	pod 'AFNetworking'
+	pod 'SVProgressHUD'
+	pod 'EstimoteSDK'
+end
+
+target 'decoTests' do
+
+end
+
+target 'decoUITests' do
+
+end
+
---
+++ deco/deco-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  deco-Bridging-Header.h
+//  deco
+//
+//  Created by Shinnosuke Komiya on 2015/12/13.
+//  Copyright © 2015年 DesignCat Inc. All rights reserved.
+//
+
+#ifndef deco_Bridging_Header_h
+#define deco_Bridging_Header_h
+
+#import "SVProgressHUD.h"
+#import "AFNetworking.h"
+#import "EstimoteSDK.h"
+
+
+#endif /* deco_Bridging_Header_h */
